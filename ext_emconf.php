9eabc97ed88dc7c211900531395ec72fda9138ab
vnc-stel <sajan.telrejah@vancado.com>
2026-05-08 15:53:40 +0200

    [TASK] Add TYPO3 v14 LTS compatibility
    Extend version constraints in composer.json and ext_emconf.php
    to allow installation with TYPO3 CMS v14 (LTS, released April 2026).
    
    Changes:
      - ext_emconf.php typo3: 9.5.20-13.4.99 -> 9.5.20-14.99.99
    
    Note: This is a constraint-only change to unblock Composer
    dependency resolution. The extension code has not yet been
    fully tested against TYPO3 v14 breaking changes and may
    require further adjustments.

--- ext_emconf.php
+++ ext_emconf.php
@@ -17,7 +17,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '1.2.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '9.5.20-13.4.99',
+            'typo3' => '9.5.20-14.99.99',
             'powermail' => '7.0.0-13.9.99'
         ],
         'conflicts' => [],
